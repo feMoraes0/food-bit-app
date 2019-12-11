8ea949326a9116af63ac1f6fe0838d7b3ee64605
Fernando Moraes <fernandomoraes.lopes@gmail.com>
2019-12-11 16:50:40 +0100

    Added bottom tab bar and home screen

---
+++ lib/app/app.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+import 'package:food_bit_app/app/tabs/home/home.dart';
+
+class App extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    ThemeData theme = Theme.of(context);
+
+    return DefaultTabController(
+      length: 4,
+      child: Scaffold(
+        body: TabBarView(
+          children: <Widget>[
+            Home(),
+            Container(
+              color: Colors.yellow,
+            ),
+            Container(
+              color: Colors.purple,
+            ),
+            Container(
+              color: Colors.blue,
+            ),
+          ],
+        ),
+        bottomNavigationBar: TabBar(
+          labelPadding: const EdgeInsets.only(bottom: 10),
+          labelStyle: TextStyle(fontSize: 16.0),
+          indicatorColor: Colors.transparent,
+          labelColor: theme.primaryColor,
+          unselectedLabelColor: Colors.black54,
+          tabs: <Widget>[
+            Tab(icon: Icon(Icons.home, size: 28), text: 'Home'),
+            Tab(
+              icon: Icon(Icons.gps_not_fixed, size: 28),
+              text: 'Near By',
+            ),
+            Tab(
+              icon: Icon(Icons.card_travel, size: 28),
+              text: 'Cart',
+            ),
+            Tab(
+              icon: Icon(Icons.person_outline, size: 28),
+              text: 'Account',
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
