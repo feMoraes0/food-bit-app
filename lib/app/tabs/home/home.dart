6c8c094df4dcfd666d2f395a61b7fe92ef3a3a0f
Fernando Moraes <fernandomoraes.lopes@gmail.com>
2019-12-11 17:22:27 +0100

    Started home development and added icon images

--- lib/app/tabs/home/home.dart
+++ lib/app/tabs/home/home.dart
@@ -3,8 +3,50 @@ import 'package:flutter/material.dart';
 class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.deepOrange,
+    ThemeData theme = Theme.of(context);
+
+    return SafeArea(
+      child: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget>[
+                Text(
+                  'What would you like to eat?',
+                  style: TextStyle(fontSize: 21.0),
+                ),
+                Icon(Icons.notifications_none, size: 28.0)
+              ],
+            ),
+            Padding(
+              padding: const EdgeInsets.only(top: 25.0),
+              child: TextField(
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  prefixIcon: Icon(
+                    Icons.search,
+                    size: 28.0,
+                    color: theme.primaryColor,
+                  ),
+                  suffixIcon: Icon(
+                    Icons.filter_list,
+                    size: 28.0,
+                    color: theme.primaryColor,
+                  ),
+                  hintText: 'Find a food or Restaurant',
+                  hintStyle: TextStyle(
+                    color: Colors.grey[400],
+                    fontSize: 19.0,
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
