a83dec49e6fff41c372b0323f8a13b87e2eea078
Fernando Moraes <fernandomoraes.lopes@gmail.com>
2019-12-14 01:32:27 +0100

    [refactoring] stateful details to stateless details component

--- lib/app/tabs/home/details/details.dart
+++ lib/app/tabs/home/details/details.dart
@@ -1,36 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:food_bit_app/app/tabs/home/details/body_details.dart';
 
-Widget iconBadge({@required IconData icon, @required Color iconColor}) {
-  return Container(
-    padding: const EdgeInsets.all(4.0),
-    decoration: BoxDecoration(
-      boxShadow: [
-        BoxShadow(
-          color: Colors.black12,
-          blurRadius: 4.0,
-          offset: Offset(3.0, 3.0),
-        )
-      ],
-      shape: BoxShape.circle,
-      color: Colors.white,
-    ),
-    child: Icon(
-      icon,
-      size: 20.0,
-      color: iconColor,
-    ),
-  );
-}
-
-class Details extends StatefulWidget {
-  @override
-  _DetailsState createState() => _DetailsState();
-}
-
-class _DetailsState extends State<Details> with TickerProviderStateMixin {
-  int screenTab = 0;
-
+class Details extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
