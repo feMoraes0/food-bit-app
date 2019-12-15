6753e3992028826e1c93def03a51a22bc9116c6b
Fernando Moraes <fernandomoraes.lopes@gmail.com>
2019-12-15 03:43:14 +0100

    Removed card to review tab

--- lib/app/tabs/home/details/body_details.dart
+++ lib/app/tabs/home/details/body_details.dart
@@ -96,54 +96,52 @@ Widget detailsTab() {
 }
 
 Widget renderCardReview() {
-  return Card(
-    child: Padding(
-      padding: const EdgeInsets.all(8.0),
-      child: Column(
-        children: <Widget>[
-          Row(
-            children: <Widget>[
-              CircleAvatar(
-                child: Icon(Icons.person),
+  return Padding(
+    padding: const EdgeInsets.all(8.0),
+    child: Column(
+      children: <Widget>[
+        Row(
+          children: <Widget>[
+            CircleAvatar(
+              child: Icon(Icons.person),
+            ),
+            Container(
+              margin: const EdgeInsets.only(left: 10.0),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Text(
+                    'Person',
+                    style: TextStyle(
+                      fontSize: 18.0,
+                      color: Colors.black87,
+                    ),
+                  ),
+                  Text(
+                    'December 14, 2019',
+                    style: TextStyle(
+                      color: Colors.grey[400],
+                    ),
+                  ),
+                ],
               ),
-              Container(
-                margin: const EdgeInsets.only(left: 10.0),
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: <Widget>[
-                    Text(
-                      'Person',
-                      style: TextStyle(
-                        fontSize: 18.0,
-                        color: Colors.black87,
-                      ),
-                    ),
-                    Text(
-                      'December 14, 2019',
-                      style: TextStyle(
-                        color: Colors.grey[400],
-                      ),
-                    ),
-                  ],
-                ),
-              )
-            ],
+            )
+          ],
+        ),
+        Container(
+          margin: const EdgeInsets.only(
+            left: 50.0,
+            top: 2.0,
           ),
-          Container(
-            margin: const EdgeInsets.only(
-              left: 50.0,
-              top: 2.0,
+          child: Text(
+            'Cras ac nunc pretium, lacinia lorem ut, congue metus. Aenean vitae lectus at mauris eleifend placerat. Proin a nisl ut risus euismod ultrices et sed dui.',
+            style: TextStyle(
+              fontSize: 13.0,
             ),
-            child: Text(
-              'Cras ac nunc pretium, lacinia lorem ut, congue metus. Aenean vitae lectus at mauris eleifend placerat. Proin a nisl ut risus euismod ultrices et sed dui.',
-              style: TextStyle(
-                fontSize: 13.0,
-              ),
-            ),
-          )
-        ],
-      ),
+          ),
+        )
+      ],
     ),
   );
 }
