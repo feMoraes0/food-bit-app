ded28439596b21d5a6f3755413b6706cd4fb64b7
Fernando Moraes <fernandomoraes.lopes@gmail.com>
2019-12-15 03:52:57 +0100

    Started account tab with circle avatar

--- lib/app/tabs/account/account.dart
+++ lib/app/tabs/account/account.dart
@@ -5,8 +5,22 @@ class Account extends StatelessWidget {
   Widget build(BuildContext context) {
     return SafeArea(
       child: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
         children: <Widget>[
-          Text('Profile'),
+          Text(
+            'Profile',
+            style: TextStyle(fontSize: 24.0),
+            textAlign: TextAlign.center,
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 20.0),
+            child: CircleAvatar(
+              backgroundColor: Colors.blueGrey,
+              radius: 60.0,
+              child: Icon(Icons.person_outline, size: 55.0, color: Colors.white,),
+            ),
+          ),
         ],
       ),
     );
