bab0784d3904dbd441d04aed4ae71897f4b50654
Fernando Moraes <fernandomoraes.lopes@gmail.com>
2019-12-16 02:53:02 +0100

    Added favourite food tab

--- lib/app/tabs/account/account.dart
+++ lib/app/tabs/account/account.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:food_bit_app/app/components/food_card.dart';
 
 class Account extends StatefulWidget {
   @override
@@ -7,17 +8,45 @@ class Account extends StatefulWidget {
 
 class _AccountState extends State<Account> with TickerProviderStateMixin {
   TabController _tabController;
+  final List<Map<String, String>> favoriteFoods = [
+    {
+      'name': 'Tandoori Chicken',
+      'price': '96.00',
+      'rate': '4.9',
+      'clients': '200',
+      'image': 'images/plate-001.png'
+    },
+    {
+      'name': 'Salmon',
+      'price': '40.50',
+      'rate': '4.5',
+      'clients': '168',
+      'image': 'images/plate-002.png'
+    },
+    {
+      'name': 'Rice and meat',
+      'price': '130.00',
+      'rate': '4.8',
+      'clients': '150',
+      'image': 'images/plate-003.png'
+    }
+  ];
 
   @override
   void initState() {
-    this._tabController =
-        TabController(length: 2, initialIndex: 0, vsync: this);
+    this._tabController = TabController(
+      length: 2,
+      initialIndex: 0,
+      vsync: this,
+    );
     super.initState();
   }
 
   @override
   Widget build(BuildContext context) {
     ThemeData theme = Theme.of(context);
+    Size size = MediaQuery.of(context).size;
+
     return SafeArea(
       child: Column(
         mainAxisAlignment: MainAxisAlignment.start,
@@ -65,7 +94,10 @@ class _AccountState extends State<Account> with TickerProviderStateMixin {
                 ),
                 Text(
                   'Warsaw, Poland',
-                  style: TextStyle(fontSize: 16.0, color: Colors.black54,),
+                  style: TextStyle(
+                    fontSize: 16.0,
+                    color: Colors.black54,
+                  ),
                 ),
               ],
             ),
@@ -135,27 +167,47 @@ class _AccountState extends State<Account> with TickerProviderStateMixin {
               ],
             ),
           ),
-          TabBar(
-            controller: this._tabController,
-            indicatorColor: theme.primaryColor,
-            indicatorSize: TabBarIndicatorSize.label,
-            labelColor: theme.primaryColor,
-            labelStyle: TextStyle(fontSize: 20.0),
-            unselectedLabelColor: Colors.black,
-            tabs: <Widget>[
-              Tab(text: 'Your Favorite'),
-              Tab(text: 'Account Setting'),
-            ],
+          Container(
+            color: Colors.white,
+            child: TabBar(
+              controller: this._tabController,
+              indicatorColor: theme.primaryColor,
+              indicatorSize: TabBarIndicatorSize.label,
+              labelColor: theme.primaryColor,
+              labelStyle: TextStyle(fontSize: 20.0),
+              unselectedLabelColor: Colors.black,
+              tabs: <Widget>[
+                Tab(text: 'Your Favorite'),
+                Tab(text: 'Account Setting'),
+              ],
+            ),
           ),
           Expanded(
               child: TabBarView(
             controller: this._tabController,
             children: <Widget>[
               Container(
-                color: Colors.redAccent,
+                child: GridView.count(
+                  crossAxisCount: 2,
+                  childAspectRatio: ((size.width / 2) / 230),
+                  children: this.favoriteFoods.map((product) {
+                    return Container(
+                      margin: const EdgeInsets.only(top: 10.0),
+                      child: FoodCard(
+                          width: size.width,
+                          primaryColor: theme.primaryColor,
+                          productName: product['name'],
+                          productPrice: product['price'],
+                          productUrl: product['image'],
+                          productClients: product['clients'],
+                          productRate: product['rate'],
+                        ),
+                    );
+                  }).toList(),
+                ),
               ),
               Container(
-                color: Colors.blue,
+                color: Colors.red,
               ),
             ],
           ))
