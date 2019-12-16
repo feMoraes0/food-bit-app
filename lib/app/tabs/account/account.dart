bad893bf93a1ea3129ebfe39f8d911214620a03d
Fernando Moraes <fernandomoraes.lopes@gmail.com>
2019-12-16 02:04:27 +0100

    Added profile scaffold

--- lib/app/tabs/account/account.dart
+++ lib/app/tabs/account/account.dart
@@ -1,8 +1,23 @@
 import 'package:flutter/material.dart';
 
-class Account extends StatelessWidget {
+class Account extends StatefulWidget {
+  @override
+  _AccountState createState() => _AccountState();
+}
+
+class _AccountState extends State<Account> with TickerProviderStateMixin {
+  TabController _tabController;
+
+  @override
+  void initState() {
+    this._tabController =
+        TabController(length: 2, initialIndex: 0, vsync: this);
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
+    ThemeData theme = Theme.of(context);
     return SafeArea(
       child: Column(
         mainAxisAlignment: MainAxisAlignment.start,
@@ -13,14 +28,137 @@ class Account extends StatelessWidget {
             style: TextStyle(fontSize: 24.0),
             textAlign: TextAlign.center,
           ),
-          Padding(
-            padding: const EdgeInsets.symmetric(vertical: 20.0),
+          Container(
+            margin: const EdgeInsets.only(
+              top: 20.0,
+              bottom: 15.0,
+            ),
             child: CircleAvatar(
               backgroundColor: Colors.blueGrey,
-              radius: 60.0,
-              child: Icon(Icons.person_outline, size: 55.0, color: Colors.white,),
+              radius: 46.0,
+              child: Icon(
+                Icons.person,
+                size: 55.0,
+                color: Colors.white,
+              ),
             ),
           ),
+          Text(
+            'Fernando Moraes',
+            textAlign: TextAlign.center,
+            style: TextStyle(
+              fontSize: 25.0,
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(5.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 5.0),
+                  child: Icon(
+                    Icons.location_on,
+                    size: 20.0,
+                    color: Colors.black54,
+                  ),
+                ),
+                Text(
+                  'Warsaw, Poland',
+                  style: TextStyle(fontSize: 16.0, color: Colors.black54,),
+                ),
+              ],
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(
+              vertical: 25.0,
+              horizontal: 5.0,
+            ),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: <Widget>[
+                Column(
+                  children: <Widget>[
+                    Text(
+                      '250K',
+                      style: TextStyle(
+                        fontSize: 18.0,
+                        color: theme.primaryColor,
+                      ),
+                    ),
+                    Text(
+                      'Follower',
+                      style: TextStyle(
+                        fontSize: 18.0,
+                        color: Colors.grey[500],
+                      ),
+                    ),
+                  ],
+                ),
+                Column(
+                  children: <Widget>[
+                    Text(
+                      '500',
+                      style: TextStyle(
+                        fontSize: 18.0,
+                        color: theme.primaryColor,
+                      ),
+                    ),
+                    Text(
+                      'Following',
+                      style: TextStyle(
+                        fontSize: 18.0,
+                        color: Colors.grey[500],
+                      ),
+                    ),
+                  ],
+                ),
+                Column(
+                  children: <Widget>[
+                    Text(
+                      '540',
+                      style: TextStyle(
+                        fontSize: 18.0,
+                        color: theme.primaryColor,
+                      ),
+                    ),
+                    Text(
+                      'Taste Master',
+                      style: TextStyle(
+                        fontSize: 18.0,
+                        color: Colors.grey[500],
+                      ),
+                    ),
+                  ],
+                )
+              ],
+            ),
+          ),
+          TabBar(
+            controller: this._tabController,
+            indicatorColor: theme.primaryColor,
+            indicatorSize: TabBarIndicatorSize.label,
+            labelColor: theme.primaryColor,
+            labelStyle: TextStyle(fontSize: 20.0),
+            unselectedLabelColor: Colors.black,
+            tabs: <Widget>[
+              Tab(text: 'Your Favorite'),
+              Tab(text: 'Account Setting'),
+            ],
+          ),
+          Expanded(
+              child: TabBarView(
+            controller: this._tabController,
+            children: <Widget>[
+              Container(
+                color: Colors.redAccent,
+              ),
+              Container(
+                color: Colors.blue,
+              ),
+            ],
+          ))
         ],
       ),
     );
